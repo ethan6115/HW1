da6249eda91ea59cddda9d491d07d2b06dc6ad1d
李懿宸 <83497453+ethan6115@users.noreply.github.com>
2024-10-14 18:20:42 +0800

    Create climbStairs.s

---
+++ climbStairs.s
@@ -0,0 +1,37 @@
+    .data
+n:      .word 30                 # Input value n
+
+    .text
+    .globl main
+main:
+    lw a0, n                    # Load n into a0
+    li t0, 1                    # first = 1 (ways to reach step 1)
+    li t1, 2                    # second = 2 (ways to reach step 2)
+
+    beq a0, t1, return_two       # If n == 2, return 2
+    beq a0, t0, return_one       # If n == 1, return 1
+    
+    li t2, 2                    # Initialize i to 2
+
+loop:
+    add t3, t0, t1              # ans = a + b
+    addi t0, t1, 0                 # a = b
+    addi t1, t3, 0                 # b = ans
+    addi t2, t2, 1              # Increment i
+    blt t2, a0, loop            # If i < n, go to loop
+
+return_result:
+    addi a0, t3, 0                 # Move result to a0
+    j print_result
+    
+return_one:
+    li a0, 1                    # Return 1
+    j print_result
+
+return_two:
+    li a0, 2                    # Return 2
+    j print_result
+
+print_result:
+    li a7, 1                    # Prepare to print result
+    ecall                       # Print the result
